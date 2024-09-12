4527dedee8f7531eea969892f9ddb0bf1ff2a643
JAKE CHAPUT <JWC21@FSU.EDU>
2024-09-12 14:29:47 -0400

    blue text that says "House" on screen

--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
+let courierFont
+
+function preload() {
+  courierFont = loadFont("assets/CourierPrime-Regular.ttf");
+}
+
 function setup() {
   createCanvas(400, 400);
+  textFont(courierFont)
 }
 
 function draw() {
   background(125);
-  
+  fill("blue")
+  text("House", width/2, height/2)
 }
