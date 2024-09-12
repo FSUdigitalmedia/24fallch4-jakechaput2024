538a0f3323e834e2ff1966c7529591cc5d6f09aa
JAKE CHAPUT <JWC21@FSU.EDU>
2024-09-12 17:21:39 -0400

    "House of Leaves", text colored, size changed, in black cube

--- sketch.js
+++ sketch.js
@@ -6,13 +6,16 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  textFont(courierFont)
+  textFont(courierFont);
+  textSize(35);
 }
 
 function draw() {
   background(125);
-  fill("blue");
+  fill("black")
+  rect(89, 30, 215, 240);
+  fill(101,112,174); //changes the text color of "House" to blue, RGB color code
   text("House", 100, 100);
   fill("white");
-  text("of Leaves", 100, 115);
+  text("of Leaves", 100, 135);
 }
