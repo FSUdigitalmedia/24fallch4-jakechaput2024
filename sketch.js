f978fae1e6b3437e121f8f621745d10303957d5a
JAKE CHAPUT <JWC21@FSU.EDU>
2024-09-13 15:52:26 -0400

    added text under square

--- sketch.js
+++ sketch.js
@@ -7,15 +7,33 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textFont(courierFont);
-  textSize(35);
+  //textSize(35);
+
 }
 
 function draw() {
-  background(125);
+  background(252, 238, 215);
+
+  //test
+  //----
+  fill("black")
+  textSize(12);
+  text('Little solace comes to those who grieve', 10, 120);
+  textSize(12);
+  text('when thoughts keep drifting as walls keep shifting', 10, 140);
+  textSize(12);
+  text('and this great blue world of ours seems a house of leaves', 10, 160);
+  textSize(12);
+  text('moments before the wind', 10, 180);
+
   fill("black")
   rect(89, 30, 215, 240);
   fill(101,112,174); //changes the text color of "House" to blue, RGB color code
+  textSize(35);
   text("House", 100, 100);
   fill("white");
+  textSize(35);
   text("of Leaves", 100, 135);
+
+  
 }
